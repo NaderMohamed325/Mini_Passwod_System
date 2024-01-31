ed5decc727de4fed8047c128345ebdf1e9599976
Nader Mohamed Nader <112278447+NaderMohamed325@users.noreply.github.com>
2024-01-31 18:40:38 +0300

    Add files via upload

---
+++ app.h
@@ -0,0 +1,28 @@
+/* 
+ * File:   app.h
+ * Author: xcite
+ *
+ * Created on November 14, 2023, 8:33 PM
+ */
+#ifndef APP_H
+#define APP_H
+
+/*----------------INCLUDES-------------------*/
+#include "ECU_LAYER/ecu_layer_init.h"
+/*-----------------MACROS--------------------*/
+
+
+
+/*--------------MACROS_FUNCTIONS-----------*/
+
+
+/*------------DATA_TYPES-------------------*/
+extern lcd_4bit_mode lcd;
+extern Keypad_t key;
+extern button_t button;
+/*------------FUNCTIONS_DECLARATION--------*/
+void Application_initialize(void);
+
+
+#endif	/* APP_H */
+
